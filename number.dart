466954939b27d4d62fbf8c87be6276253969e5f9
Jostina Ndavi <122972667+jostina@users.noreply.github.com>
2024-03-08 12:41:57 +0300

    Add files via upload

---
+++ number.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+void main() {
+  stdout.write('6:');
+  String? input = stdin.readLineSync();
+  
+  if (input != null) {
+    try {
+      int number = int.parse(input);
+      
+      if (number > 10) {
+        print('Your number is greater than 10');
+      } else if (number < 10) {
+        print('Your number is less than 10');
+      } else {
+        print('Your number is equal to 10');
+      }
+    } catch (e) {
+      print('Invalid input. Please enter a valid number.');
+    }
+  }
+}
