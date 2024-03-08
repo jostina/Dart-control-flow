36ddee7202ade16365cfd8bccdf4c9fd263bb6ca
jostina <jnjndavi8@gmail.com>
2024-03-08 13:07:26 +0300

    first commit

---
+++ profile_default/data.py
@@ -0,0 +1,29 @@
+a=100
+b=3.14
+#print(a*b)
+str = 'Welcome to PLP Academy'
+#print(str)
+#print(str[0])
+#print(str[-1])
+#print(str[4:10])
+plp = "Python Class"
+#print(plp)
+#print(str + '' ,plp)
+shopping_list = ['bread','milk','jam',20,40,60,5+8j]
+#print(shopping_list)
+Building_List = [450,'Wheelbarrow',180,'shovel','bread','milk']
+#print(Building_List)
+#print(shopping_list[6:9])
+#print(shopping_list + Building_List)
+#print(shopping_list*2)
+plp_tuple = 'abcd',786,2.23,'john',70.2
+print(plp_tuple)
+tiny_tuple = ('john',5+7j)
+print(tiny_tuple)
+dict = {}
+dict['one'] = 'This is one'
+tinydict = {'name':'john','code':6734,'dept':1007}
+print(dict['one'])
+print(dict[2])
+print(tinydict)
+print(tinydict.key())
